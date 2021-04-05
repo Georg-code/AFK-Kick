81b4d107b2f9234c7fefedb344e7c9983349c021
Georg <54809379+Georg-code@users.noreply.github.com>
2021-04-05 11:24:09 +0200

    Update Command.java

--- src/ch/derhohi/main/Command.java
+++ src/ch/derhohi/main/Command.java
@@ -17,8 +17,6 @@ public class Command implements CommandExecutor {
 			boolean sond_on_notafk = Boolean.parseBoolean(Main.getInstance().getConfig().getString("Config.sond_on_notafk"));
 			if (sond_on_notafk == true) {
 				p.playSound(p.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 10,3);
-			} else {
-
 			}
 			Main main = Main.getInstance();
 			for (int i = 0; i < main.list.size(); i++) {
@@ -27,7 +25,7 @@ public class Command implements CommandExecutor {
 					break;
 				}
 			}
-			p.sendMessage("ยง7" + Main.getInstance().getConfig().getString("Config.prefix") + " " + Main.getInstance().getConfig().getString("Config.kick_not_afk_msg"));
+			p.sendMessage("รยง7" + Main.getInstance().getConfig().getString("Config.prefix") + " " + Main.getInstance().getConfig().getString("Config.kick_not_afk_msg"));
 
 			return true;
 		} else if (cmd.getName().equalsIgnoreCase("afkreload")) {
@@ -46,22 +44,22 @@ public class Command implements CommandExecutor {
 				String sound_on_notafk = Main.getInstance().getConfig().getString("Config.sond_on_notafk");
 				String config_noperm = Main.getInstance().getConfig().getString("Config.config_noperm");
 				String notafkmsg = Main.getInstance().getConfig().getString("Config.kick_not_afk_msg");
-				sender.sendMessage("ง7---------------------------------");
-				sender.sendMessage("ง3Prefix: ง7" + prefix);
-				sender.sendMessage("ง3Kickmsg: ง7" + kick_msg);
-				sender.sendMessage("ง3Kick warn delay: ง7" + kick_warn_delay);
-				sender.sendMessage("ง3Kick warn message: ง7" + kick_warn_msg);
-				sender.sendMessage("ง3Not AFK msg: ง7" + kick_warn_msg_afk);
-				sender.sendMessage("ง3Not AFK confirmed: " + notafkmsg);
-				sender.sendMessage("ง3Kick Delay: ง7" + kick_delay);
-				sender.sendMessage("ง3Sound on warn: ง7" + sound_on_get_warn);
-				sender.sendMessage("ง3Sound on confirm: ง7" + sound_on_notafk);
-				sender.sendMessage("ง3No permisson: ง7" + config_noperm);
-				sender.sendMessage("ง7---------------------------------");
+				sender.sendMessage("ยง7---------------------------------");
+				sender.sendMessage("ยง3Prefix: ยง7" + prefix);
+				sender.sendMessage("ยง3Kickmsg: ยง7" + kick_msg);
+				sender.sendMessage("ยง3Kick warn delay: ยง7" + kick_warn_delay);
+				sender.sendMessage("ยง3Kick warn message: ยง7" + kick_warn_msg);
+				sender.sendMessage("ยง3Not AFK msg: ยง7" + kick_warn_msg_afk);
+				sender.sendMessage("ยง3Not AFK confirmed: " + notafkmsg);
+				sender.sendMessage("ยง3Kick Delay: ยง7" + kick_delay);
+				sender.sendMessage("ยง3Sound on warn: ยง7" + sound_on_get_warn);
+				sender.sendMessage("ยง3Sound on confirm: ยง7" + sound_on_notafk);
+				sender.sendMessage("ยง3No permisson: ยง7" + config_noperm);
+				sender.sendMessage("ยง7---------------------------------");
 				sender.sendMessage("");
 				sender.sendMessage("" + config_reload);
 			} else {
-				sender.sendMessage("ง7" + Main.getInstance().getConfig().getString("Config.config_noperm"));
+				sender.sendMessage("ยง7" + Main.getInstance().getConfig().getString("Config.config_noperm"));
 			}
 		}
 		return true;
